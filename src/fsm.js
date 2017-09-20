ca6389a0bb02277dc2fbfa8b4a7af96e7d8c9523
marina_sakovich <sakovichm@mail.ru>
2017-09-20 17:35:32 +0300

    works currectly

--- src/fsm.js
+++ src/fsm.js
@@ -4,32 +4,18 @@ class FSM {
      * @param config
      */
     constructor(config) {
-        //error empty config
         if (arguments.length === 0) {
-            //console.log('config error');
             throw new Error();
         } else {
-            this.history = [];
-            this.index = 0;
-            this.history.push('');
+            this.prevHistory = [];
+            this.nextHistory = [];
 
             this.stateInitial = config.initial;
             this.currentState = config.initial;
             this.states = config.states;
-
-            this.history.push(this.currentState);
-            this.index++;
-    }
+        }
     }
 
-    getPreviousState() {
-        //return this.previousState;
-        return this.history[this.index];
-    }
-
-    // setPreviousState(){
-    //     this.previousState = this.currentState;
-    // }
 
     /**
      * Returns active state.
@@ -45,20 +31,13 @@ class FSM {
      */
     changeState(state) {
         if (state in this.states) {
-
-            this.currentState = state;
-
-            //this.history[this.index] = this.currentState;
-
-            this.history.push(this.currentState);
-            this.index = this.history.length - 1;
-            //this.index ++;
+            if (this.currentState != state) {
+                this.prevHistory.push(this.currentState);
+                this.currentState = state;
+            }
         }
         else{
-            console.log('error in changeState');
             throw new Error();
-            //console.log('error in changeState');
-            //add error;
         }
     }
 
@@ -77,10 +56,9 @@ class FSM {
                 for (var key2 in stateObj[key].transitions) {
                     if (key2 === event) {
 
-                        this.currentState = stateObj[key].transitions[key2];
+                        var state = stateObj[key].transitions[key2];
 
-                        this.history.push(this.currentState);
-                        this.index = this.history.length - 1;
+                        this.changeState(state);
 
                         isFound = true;
                         break;
@@ -92,13 +70,9 @@ class FSM {
             }
         }
 
-            if(isFound === false) {
-                //console.log('error in trigger');
-                //throw new Error();
-                //throw new Error();
-                //
-                //error
-            }
+        if(isFound === false) {
+            throw new Error();
+        }
     }
 
     /**
@@ -106,11 +80,6 @@ class FSM {
      */
     reset() {
         this.currentState = this.stateInitial;
-        // perhaps clearHistory()
-
-
-        //this.previousState = this.currentState;//setPreviousState();//my fun
-        //this.currentState = this.stateInitial;
     }
 
     /**
@@ -122,10 +91,13 @@ class FSM {
     getStates(event) {
         var states = [];
         if(arguments.length === 0) {
+
             for(var key in this.states) {
                 states.push(key);
             }
+
         } else {
+
             for(var key in this.states) {
                 for( var key2 in this.states[key].transitions) {
                     if(key2 === event) {
@@ -144,17 +116,15 @@ class FSM {
      * @returns {Boolean}
      */
     undo() {
-        if(this.history[this.index - 1] === '') {
+        if(this.prevHistory.length === 0) {
             return false;
         }else {
+            if(this.nextHistory[this.nextHistory.length - 1] != this.currentState) {
+                this.nextHistory.push(this.currentState);
+            }
 
-            this.currentState = this.history[this.index - 1];
-           // // this.history.push(this.currentState);
-            //// this.index ++;
-            //this.history.push(this.currentState);
-            this.index --;
-
-            //this.previousState = this.currentState;//setPreviousState();//my fun
+            this.currentState = this.prevHistory[this.prevHistory.length - 1]
+            this.prevHistory.splice(this.prevHistory.length - 1, 1);
 
             return true;
         }
@@ -166,13 +136,12 @@ class FSM {
      * @returns {Boolean}
      */
     redo() {
-        if(this.index >= this.history.length - 1) {
+        if(this.nextHistory.length === 0) {
             return false;
         }else {
-            this.currentState = this.history[this.index + 1];
-            this.index ++;
-
-            //this.previousState = this.currentState;//setPreviousState();//my fun
+            this.prevHistory.push(this.currentState);
+            this.currentState = this.nextHistory[this.nextHistory.length - 1];
+            this.nextHistory.splice(this.nextHistory.length - 1, 1);
 
             return true;
         }
@@ -182,65 +151,12 @@ class FSM {
      * Clears transition history
      */
     clearHistory() {
-        this.history = [];
-        this.history.push('');
-        this.index = 0;
+        this.prevHistory = [];
+        this.nextHistory = [];
         this.currentState = this.stateInitial;
-        this.history.push(this.currentState);
-        this.index++;
     }
 }
 
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
-
-function test(){
-    const config = {
-        initial: 'normal',
-        states: {
-            normal: {
-                transitions: {
-                    study: 'busy',
-                }
-            },
-            busy: {
-                transitions: {
-                    get_tired: 'sleeping',
-                    get_hungry: 'hungry',
-                }
-            },
-            hungry: {
-                transitions: {
-                    eat: 'normal'
-                },
-            },
-            sleeping: {
-                transitions: {
-                    get_hungry: 'hungry',
-                    get_up: 'normal',
-                },
-            },
-        }
-    };
-
-    var student = new FSM(config);
-
-    var state = student.getState();
-
-    student.trigger('study');
-    student.undo();
-
-    student.trigger('study');
-    student.undo();
-
-    student.trigger('study');
-    student.redo();
-
-    var redo = student.redo();//).to.be.false;
-
-    console.log(state, redo);
-
-}
-
-test();
